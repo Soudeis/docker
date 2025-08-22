4bcb3762a8022acdf40849a9957c3783e7edadb8
Abakoura <abakouraamir@gmail.com>
2025-08-22 12:00:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-# main.py
-from fastapi import FastAPI
+import mysql.connector
 
-app = FastAPI()
+# Connexion directe à la base MySQL
+conn = mysql.connector.connect(
+    host="mysql-image-latest.onrender.com",
+    port=3306,
+    user="mia",
+    password="1234",
+    database="ma_base"
+)
 
-@app.get("/")
-def home():
-    return {"message": "Hello World"}
+# Tester la connexion
+cursor = conn.cursor()
+cursor.execute("SELECT NOW()")  # récupère la date et l'heure actuelle
+result = cursor.fetchall()
+print("Connexion OK, date actuelle :", result)
+
+# Fermer la connexion
+cursor.close()
+conn.close()
